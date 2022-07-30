85759aea65b0b4943af507bf68e0546310bc676e
hyodoblog <yusei.hyodo@gmail.com>
2022-07-30 17:31:17 +0900

    🚀 PWAの導入

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const withPWA = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true
+  },
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: true
+})
 
 module.exports = nextConfig
---
+++ src/data/constants.ts
@@ -0,0 +1,1 @@
+export const SITE_TITLE = 'R6S応援パネル'
